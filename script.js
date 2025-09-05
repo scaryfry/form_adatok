3ebef14a51ed8bdde8bfeea39166b3461ca9b1bb
scaryfry <scaryfry@gmail.com>
2025-09-05 10:44:15 +0200

    asdf

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-document.getElementById('dataForm').addEventListener('submit', function (e) {
-    e.preventDefault();
 
-    const name = document.getElementById('name').value.trim();
+    const ul = document.querySelector('ul');
+    const surname = document.getElementById('surname').value.trim();
     const city = document.getElementById('city').value.trim();
     const zip = document.getElementById('zip').value.trim();
     const street = document.getElementById('street').value.trim();
     const houseNumber = document.getElementById('house-number').value.trim();
+    console.log(surname, city, zip)
 
-    document.getElementById('output');
-    output.innerHTML = ''; 
+    let datasArray = localStorage.getItem('datas') ? JSON.parse(localStorage.getItem('datas')) : [];
 
-    localStorage.setItem()
-});
+    datasArray.forEach(liMaker);
+
+    function submitData(){
+        datasArray.push(surname);
+        datasArray.push(city);
+        datasArray.push(zip);
+        datasArray.push(street);
+        datasArray.push(houseNumber);
+
+        localStorage.setItem('datas', JSON.stringify(datasArray));
+
+
+        // input.surname = '';
+        // input.city = '';
+        // input.zip = '';
+        // input.street = '';
+        // input.houseNumber = '';
+    }
+    function liMaker(text){
+        const li = document.createElement('li');
+        li.textContent = text;
+        ul.appendChild(li);
+    }
